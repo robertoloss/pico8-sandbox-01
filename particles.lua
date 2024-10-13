fb40b225ab736043b377f67f348bcad6ffc8c63a
Roberto Loss <robertoloss@gmail.com>
2024-10-13 23:27:36 +0200

    particles refined

--- particles.lua
+++ particles.lua
@@ -1,6 +1,6 @@
-cos, sin, del, circ, all
+cos, sin, del, circ, all, pset
 =
-cos, sin, del, circ, all
+cos, sin, del, circ, all, pset
 
 
 
@@ -9,14 +9,16 @@ function create_particle(x,y)
   spd=1+rnd(3)
   a=rnd(1)
   vx = spd*cos(a)
-  vy = spd*sin(a)
+  vy =  -4
+	local colors = { 6, 7, 8 }
+	local ci = flr(rnd(#colors)) +1
   local p={
+		init_y=y,
     x=x,
     y=y,
     vx=vx,
     vy=vy,
-    c=rnd(8)+8,
-    s=rnd(3),
+    c=colors[ci],
     t=0
   }
   add(particles,p)
@@ -27,24 +29,32 @@ function update_particle(p)
   -- update position
   p.x = p.x + p.vx
   p.y =  p.y + p.vy
-  -- slow down the particle
-  p.vx = p.vx * 0.9
-  p.vy = p.vy * 0.9
-  -- update time and delete if old
+
+  p.vx = p.vx * 0.5
+  p.vy = p.vy + 2
+	local numbers = { 2, 3, 3, 3, 4, 4, 5, 6 }
+	local index = flr(rnd(#numbers)) + 1
   p.t = p.t + 0.05
-  if p.t>1 then
+  if p.y > p.init_y + numbers[index] then
     del(particles,p)
   end
 end
 
 function draw_particle(p)
-  circ(p.x,p.y,p.s,p.c)
+  pset(p.x,p.y,p.c)
 end
+
+
+time = 0
 function _draw()
   cls()
-  create_particle(64,64)
-  for pt in all(particles) do
-    update_particle(pt)
-    draw_particle(pt)
-  end
+	if time < 15 then
+		create_particle(64,64)
+		create_particle(64,63)
+		for pt in all(particles) do
+			update_particle(pt)
+			draw_particle(pt)
+		end
+	end
+	time = time + 1
 end
